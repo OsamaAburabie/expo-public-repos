b0e4be8613f5865203dc10f2dc5f077279d9b4d0
osama jamil <osama@osamas-MacBook-Air.local>
2023-07-06 01:52:54 +0300

    basic navigation

---
+++ src/navigation/BottomTabStack.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {BottomTabsStackParamList} from './types';
+import Profile from '../screens/Profile';
+import Repositories from '../screens/Repositories';
+
+const BottomTabsStack = () => {
+  const {Navigator, Screen} =
+    createBottomTabNavigator<BottomTabsStackParamList>();
+  return (
+    <Navigator>
+      <Screen name="Repositories" component={Repositories} />
+      <Screen name="Profile" component={Profile} />
+    </Navigator>
+  );
+};
+export default BottomTabsStack;
---
+++ src/screens/Repositories/index.tsx
@@ -0,0 +1,31 @@
+import { FlatList, StyleSheet, View } from "react-native";
+import React from "react";
+import RepoCard from "../../components/RepoCard";
+import { styles, theme } from "../../constants/theme";
+
+type Props = {};
+
+const Repositories = ({}: Props) => {
+  return (
+    <View style={[styles.flex, style.container]}>
+      <FlatList
+        contentContainerStyle={styles.gap10}
+        showsVerticalScrollIndicator={false}
+        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+        renderItem={({ item }) => <RepoCard />}
+        onEndReached={() => {
+          console.log("onEndReached");
+        }}
+      />
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: theme.BACKGROUND,
+    paddingHorizontal: 16,
+  },
+});
+
+export default Repositories;
