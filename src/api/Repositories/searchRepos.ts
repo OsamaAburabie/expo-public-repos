261eeba58d7bf218b6806afb750fa51a7de5131d
osama jamil <osama@osamas-MacBook-Air.local>
2023-07-06 14:57:58 +0300

    removed the github token

--- src/api/Repositories/searchRepos.ts
+++ src/api/Repositories/searchRepos.ts
@@ -5,12 +5,7 @@ export async function searchRepos(query: string, page: number = 1) {
   const response = await axios.get<SearchReposResponse>(
     `https://api.github.com/search/repositories?q=${
       query || "open source"
-    }&page=${page}`,
-    {
-      headers: {
-        Authorization: "Bearer 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",
-      },
-    }
+    }&page=${page}`
   );
 
   return response.data.items;
